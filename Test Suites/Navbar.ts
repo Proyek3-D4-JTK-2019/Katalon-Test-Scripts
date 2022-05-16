fc940a22856c53b74ebb00d7b08479556f9cdd1f
fachdf <fachridhia.f@gmail.com>
2022-05-16 22:51:43 +0700

    :wrench: Add Test Suites

---
+++ Test Suites/Navbar.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Navbar</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d6c70294-76a0-4fdb-b3a1-9e56a47e1f3b</testSuiteGuid>
+</TestSuiteEntity>
